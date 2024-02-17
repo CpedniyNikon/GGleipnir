563c30e4673a0f0d9cad6476e9cd1ef98a60d265
elyutinmaks <elyutin15@gmail.com>
2024-02-17 21:08:08 +0300

    init back commit

---
+++ ggleipnir-back/src/main/kotlin/ggleipnir/back/ggleipnirback/GgleipnirBackApplication.kt
@@ -0,0 +1,11 @@
+package ggleipnir.back.ggleipnirback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GgleipnirBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<GgleipnirBackApplication>(*args)
+}
---
+++ ggleipnir-back/src/test/kotlin/ggleipnir/back/ggleipnirback/GgleipnirBackApplicationTests.kt
@@ -0,0 +1,13 @@
+package ggleipnir.back.ggleipnirback
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GgleipnirBackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
