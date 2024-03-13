a72e16e08166b36b07440d538065874e7ab7ee42
CpedniyNikon <carry2002life@mail.ru>
2024-03-13 19:32:53 +0300

    final media commit

--- ggleipnir-back/media/routes/upload_form.js
+++ ggleipnir-back/media/routes/upload_form.js
@@ -1,5 +1,7 @@
 const {minioClient} = require('../config')
 
+const crypto = require('crypto');
+
 const express = require('express')
 const router = express.Router()
 
@@ -7,19 +9,13 @@ const router = express.Router()
 const Multer = require("multer");
 const storage = Multer.memoryStorage();
 const upload = Multer({storage: storage});
-
 const path = require('path');
-
-
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../templates/upload.html'));
-});
 
 router.post('/', upload.single('photo'), (req, res) => {
     if (!req.file) {
         res.status(400).send('No file uploaded');
     } else {
-        minioClient.putObject("test", req.file.originalname, req.file.buffer)
+        minioClient.putObject("test", crypto.randomUUID(), req.file.buffer)
             .catch((e) => {
                 console.log("Error while creating object: ", e);
                 throw e;
@@ -27,6 +23,10 @@ router.post('/', upload.single('photo'), (req, res) => {
         console.log("Object uploaded successfully");
     }
     res.redirect('http://localhost:8090/upload');
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/upload.html'));
 });
 
 
